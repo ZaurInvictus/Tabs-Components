18a2881570a7b0ef8564809d96033a7c46f0b177
Zaur <42130952+ZaurInvictus@users.noreply.github.com>
2019-05-09 20:35:27 -0500

    added random image functionality to carousel

--- components/Carousel/carousel.js
+++ components/Carousel/carousel.js
@@ -1,5 +1,7 @@
 
-const images = [];	
+let i = 0; 			// Start Point
+const images = [];	// Images Array
+const time = 1000;	// Time Between Switch
 
 images[0] = "http://lorempixel.com/400/200/animals";
 images[1] = "http://lorempixel.com/400/200/sports";
@@ -9,6 +11,7 @@ images[2] = "http://lorempixel.com/400/200/food";
 const animal = document.querySelector('.animal');
 const sport = document.querySelector('.sport');
 const food = document.querySelector('.food');
+const random = document.querySelector('.random');
 
 animal.addEventListener('click', () => {
   document.slide.src = images[0];
@@ -22,5 +25,27 @@ food.addEventListener('click', () => {
   document.slide.src = images[2];
 });
 
+
+random.addEventListener('click', () => {
+ changeImg();
+});
+
+
+// Change Image
+function changeImg(){
+	document.slide.src = images[i];
+
+	// Check If Index Is Under Max
+	if(i < images.length - 1){
+	  // Add 1 to Index
+	  i++; 
+	} else { 
+		// Reset Back To O
+		i = 0;
+	}
+
+	// Run function every x seconds
+	setTimeout("changeImg()", time);
+}
 
 
